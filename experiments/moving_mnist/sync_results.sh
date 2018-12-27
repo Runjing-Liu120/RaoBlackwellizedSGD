2bf33a6a40bb5a7c865300b4b8ae018922deb60d
Runjing <runjing_liu@berkeley.edu>
2018-12-27 16:51:59 -0500

    first true location results

--- experiments/moving_mnist/sync_results.sh
+++ experiments/moving_mnist/sync_results.sh
@@ -1,4 +1,4 @@
-rsync -avL --progress -e 'ssh -i ./../../../bryans_key_oregon.pem' \
-   ubuntu@ec2-18-237-239-60.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/discrete_vae_experimentation/moving_mnist_experiments/mnist_vae_results/.\
-   /home/runjing_liu/Documents/astronomy/discrete_vae_experimentation/moving_mnist_experiments/mnist_vae_results/.
+rsync -avL --progress -e 'ssh -i ./../../../../bryans_key_oregon.pem' \
+   ubuntu@ec2-18-237-239-60.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/RaoBlackwellizedSGD/experiments/moving_mnist/mnist_vae_results/. \
+   /home/runjing_liu/Documents/astronomy/RaoBlackwellizedSGD/experiments/moving_mnist/mnist_vae_results/.
 
