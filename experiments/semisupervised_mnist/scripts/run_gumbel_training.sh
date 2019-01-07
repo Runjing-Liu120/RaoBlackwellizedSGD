3ae1b58fba656e4747a3ed270805ee42819ebe95
Runjing <runjing_liu@berkeley.edu>
2019-01-07 12:27:06 -0500

    scripts to run gumbel softmax training

---
+++ experiments/semisupervised_mnist/scripts/run_gumbel_training.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python ../run_gumbel_vae_training.py \
+			--epochs 100 \
+			--seed 901 \
+			--save_every 20 \
+			--outdir '../mnist_vae_results/'\
+			--outfilename 'gumbel_vae' \
+			--propn_sample 1.0 \
+			--learning_rate 1e-3 \
+			--use_vae_init True \
+			--vae_init_file '../mnist_vae_results/warm_startsvae_final' \
+			--use_classifier_init True \
+			--classifier_init_file '../mnist_vae_results/warm_startsclassifier_final'
