5d3f87683bd2dbed83230f0493d011e8fc94d717
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2019-01-16 23:00:19 +0000

    gumbel training

---
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_gumbel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python ../run_moving_mnist_training.py \
+			--epochs 50 \
+			--batch_size 64 \
+			--seed 901 \
+			--outdir '../mnist_vae_results/'\
+			--outfilename 'moving_mnist_vae_gumbel' \
+			--grad_estimator 'rebar' \
+			--propn_sample 0.1 \
+			--learning_rate 1e-3 \
+			--save_every 20 \
+			--print_every 10 \
+			--topk 0
