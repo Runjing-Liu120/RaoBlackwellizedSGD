8c44bf3452df5e5537e6cb6e9a6481d4704dc1b0
Runjing <runjing_liu@berkeley.edu>
2019-01-16 15:06:08 -0800

    scripts to run

--- experiments/moving_mnist/scripts/run_moving_mnist_training_reinforce.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_reinforce.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 python ../run_moving_mnist_training.py \
-			--epochs 50 \
+			--epochs 100 \
 			--batch_size 64 \
-			--seed 992 \
+			--seed 901 \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'test' \
+			--outfilename 'moving_mnist_vae_reinforce_double_bs' \
+			--grad_estimator 'reinforce_double_bs'
 			--propn_sample 0.1 \
 			--learning_rate 1e-3 \
 			--save_every 20 \
-			--print_every 10 \
+			--print_every 5 \
 			--topk 0 \
 			--n_samples 1 \
-			--use_vae_init False \
-			--vae_init_file '../mnist_vae_results/moving_mnist_vae_topk5_final' 
--- experiments/moving_mnist/scripts/run_moving_mnist_training_topk.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_topk.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 python ../run_moving_mnist_training.py \
-			--epochs 50 \
+			--epochs 100 \
 			--batch_size 64 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'moving_mnist_vae_reinforce_doublebs_topk3' \
+			--outfilename 'moving_mnist_vae_reinforce_doublebs_topk5' \
 			--grad_estimator 'reinforce_double_bs'\
 			--propn_sample 0.1 \
 			--learning_rate 1e-3 \
 			--save_every 20 \
-			--print_every 10 \
-			--topk 5 \
-			--use_vae_init False \
-			--vae_init_file '../mnist_vae_results/moving_mnist_vae_topk5_final' 
+			--print_every 5 \
+			--topk 5 
