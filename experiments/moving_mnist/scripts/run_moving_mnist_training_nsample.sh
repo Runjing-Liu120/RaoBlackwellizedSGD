1d27dda827d328a06b33c0aabae77350346b5587
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2019-01-16 05:33:46 +0000

    experimenting

--- experiments/moving_mnist/scripts/run_moving_mnist_training_nsample.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_nsample.sh
@@ -5,10 +5,12 @@ python ../run_moving_mnist_training.py \
 			--batch_size 64 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'moving_mnist_vae_rebar' \
-			--grad_estimator 'rebar' \
+			--outfilename 'moving_mnist_vae_nsample2' \
 			--propn_sample 0.1 \
-			--learning_rate 1e-5 \
+			--learning_rate 1e-3 \
 			--save_every 20 \
 			--print_every 10 \
-			--topk 0
+			--topk 0 \
+			--n_samples 2 \
+			--use_vae_init True \
+			--vae_init_file '../mnist_vae_results/moving_mnist_vae_topk5_final' 
--- experiments/moving_mnist/scripts/run_moving_mnist_training_topk.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_topk.sh
@@ -5,9 +5,11 @@ python ../run_moving_mnist_training.py \
 			--batch_size 64 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'moving_mnist_vae_topk5' \
+			--outfilename 'moving_mnist_vae_topk1' \
 			--propn_sample 0.1 \
 			--learning_rate 1e-3 \
 			--save_every 20 \
 			--print_every 10 \
-			--topk 5 
+			--topk 1 \
+			--use_vae_init True \
+			--vae_init_file '../mnist_vae_results/moving_mnist_vae_topk5_final' 
