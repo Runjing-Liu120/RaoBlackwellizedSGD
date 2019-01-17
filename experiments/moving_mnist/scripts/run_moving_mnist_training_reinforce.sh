6e58bacb12b6e45c11f86a755a6232b793e1ec6c
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2019-01-17 01:17:09 +0000

    fix in scripts

--- experiments/moving_mnist/scripts/run_moving_mnist_training_reinforce.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_reinforce.sh
@@ -6,7 +6,7 @@ python ../run_moving_mnist_training.py \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
 			--outfilename 'moving_mnist_vae_reinforce_double_bs' \
-			--grad_estimator 'reinforce_double_bs'
+			--grad_estimator 'reinforce_double_bs' \
 			--propn_sample 0.1 \
 			--learning_rate 1e-3 \
 			--save_every 20 \
