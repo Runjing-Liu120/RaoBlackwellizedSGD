64bded046f041d40fb90108d59e9a1e47ffd2397
Runjing <runjing_liu@berkeley.edu>
2019-01-18 16:36:28 -0800

    tuning scripts

--- experiments/semisupervised_mnist/sync_results.sh
+++ experiments/semisupervised_mnist/sync_results.sh
@@ -1,4 +1,4 @@
 rsync -avL --progress -e 'ssh -i ./../../../../bryans_key_oregon.pem' \
-   ubuntu@ec2-54-69-216-30.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/RaoBlackwellizedSGD/experiments/semisupervised_mnist/mnist_vae_results/. \
+   ubuntu@ec2-54-190-2-71.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/RaoBlackwellizedSGD/experiments/semisupervised_mnist/mnist_vae_results/. \
    /home/runjing_liu/Documents/astronomy/RaoBlackwellizedSGD/experiments/semisupervised_mnist/mnist_vae_results/.
 
---
+++ experiments/semisupervised_mnist/tuning_scripts/run_reinforce_training.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python ../run_semisuper_vae_training.py \
+			--epochs 100 \
+			--seed 901 \
+			--save_every 20 \
+			--print_every 5 \
+			--outdir '../mnist_vae_results/'\
+			--outfilename 'ss_vae_reinforce' \
+			--propn_sample 1.0 \
+			--learning_rate 1e-3 \
+			--topk 0 \
+			--grad_estimator 'reinforce' \
+			--use_vae_init True \
+			--vae_init_file '../mnist_vae_results/warm_starts_vae_final' \
+			--use_classifier_init True \
+			--classifier_init_file '../mnist_vae_results/warm_starts_classifier_final'
