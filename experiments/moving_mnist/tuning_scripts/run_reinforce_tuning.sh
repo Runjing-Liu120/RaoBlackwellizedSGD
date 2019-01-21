3144883e7cc93fd6f89d258c534f1329b1c2ecab
Runjing <runjing_liu@berkeley.edu>
2019-01-20 17:39:16 -0800

    script to tune reinforce

--- experiments/moving_mnist/tuning_scripts/run_reinforce_tuning.sh
+++ experiments/moving_mnist/tuning_scripts/run_reinforce_tuning.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+for lr in 5e-3 1e-3 5e-4 1e-4 5e-5
+do
 python ../run_moving_mnist_training.py \
 			--epochs 50 \
 			--batch_size 64 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'moving_mnist_vae_reinforce_double_bs' \
+			--outfilename moving_mnist_vae_reinforce_double_bs_lr${lr} \
 			--grad_estimator 'reinforce_double_bs' \
 			--propn_sample 0.1 \
-			--learning_rate 1e-3 \
-			--save_every 20 \
-			--print_every 2 \
+			--learning_rate ${lr} \
+			--save_every 200 \
+			--print_every 20 \
 			--topk 0 \
 			--n_samples 1
+done
