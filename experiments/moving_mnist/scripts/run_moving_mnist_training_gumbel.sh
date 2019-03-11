13b7d59cffa969b192dcf635eeb68d48607c9753
Ubuntu <ubuntu@ip-172-31-11-181.us-west-2.compute.internal>
2019-03-11 23:36:50 +0000

    lost training script

--- experiments/moving_mnist/scripts/run_moving_mnist_training_gumbel.sh
+++ experiments/moving_mnist/scripts/run_moving_mnist_training_gumbel.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+for i in {1..10}
+do
+((seed=$i + 11129))
+
 python ../run_moving_mnist_training.py \
-			--epochs 100 \
+			--epochs 50 \
 			--batch_size 64 \
-			--seed 901 \
+			--seed $seed \
 			--outdir '../mnist_vae_results/'\
-			--outfilename 'moving_mnist_vae_gumbel' \
+			--outfilename moving_mnist_vae_gumbel_trial$i \
 			--grad_estimator 'gumbel' \
 			--propn_sample 0.1 \
-			--learning_rate 1e-3 \
-			--save_every 20 \
-			--print_every 100 \
+			--learning_rate 5e-5 \
+			--gumbel_anneal_rate 5e-4 \
+			--save_every 233 \
+			--print_every 2 \
 			--topk 0
+done
