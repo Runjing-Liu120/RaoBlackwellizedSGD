4a8c78fb93cb3dbc705efe0c869f33e465e51e35
Runjing <runjing_liu@berkeley.edu>
2019-03-12 20:18:44 -0700

    tuning rebar

--- experiments/semisupervised_mnist/tuning_scripts/run_rebar_tuning.sh
+++ experiments/semisupervised_mnist/tuning_scripts/run_rebar_tuning.sh
@@ -2,15 +2,13 @@
 
 for lr in 5e-3 1e-3 5e-4 1e-4 5e-5
 do
-	for eta in 1.2 1.4 1.6 1.8 2.0
-	do
 		python ../run_semisuper_vae_training.py \
 					--epochs 10 \
 					--seed 901 \
 					--save_every 1000 \
 					--print_every 10 \
-					--outdir '../mnist_vae_results/tuning_results/'\
-					--outfilename ss_vae_rebar_lr${lr}_eta${eta}\
+					--outdir '../mnist_vae_results_rebar/tuning_results/'\
+					--outfilename ss_vae_rebar_lr${lr}\
 					--learning_rate $lr \
 					--rebar_eta $eta \
 					--topk 0 \
